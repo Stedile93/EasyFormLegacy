30b946fe570a60d571b7102d4569e1b3f4f008cc
Giuliano Stedile <giu_stedile@hotmail.com>
2015-01-16 17:30:38 -0200

    jasmine

---
+++ spec/easyform_spec.js
@@ -0,0 +1,15 @@
+
+
+describe('Testes - EasyForm plugin', function() {
+
+	it ('Chama o plugin', function() {
+
+		var object = $('#teste').easyform();
+
+		expect(object).not.toBeUndefined();
+
+	});
+
+	/* ..... */
+
+});
